f7a703dad29e7b5c33278e9a7715de7cca5af6bd
lhg0529 <lhg0529@naver.com>
2024-03-08 20:42:05 +0900

    초기 코드

---
+++ src/variable/Var1.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //변경 10 -> 20
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(20);
+        }
+    }
+}
